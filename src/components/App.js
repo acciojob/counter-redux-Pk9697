ae5dd2923dac8daad6e3a9295301fae5528dd70f
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-10 13:49:14 +0530

    Check testcases

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,10 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>0</h1>
+      <button>+</button>
+      <button>-</button>
     </div>
   )
 }
